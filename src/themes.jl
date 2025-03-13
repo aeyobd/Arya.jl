dd9387da53a665f5629d706d8af5133f40c6b3d9
Daniel Boyea <aeyobd@gmail.com>
2025-03-13 10:38:10 -0700

    removes unneccessary ouput and defaults to svg with cairomakie

--- src/themes.jl
+++ src/themes.jl
@@ -2,6 +2,7 @@ import TOML
 import Colors: Colorant
 import ColorSchemeTools: make_colorscheme
 
+
 struct SmartMinorTicks end
 
 const UNITS_PER_INCH = 72
@@ -54,7 +55,7 @@ function theme_arya(; width=3.25, fontsize=12)
             make_font_settings(fontsize=fontsize)...
         ),
 
-        CairoMakie = (; px_per_unit=5, type="png"),
+        CairoMakie = (; px_per_unit=5, type="svg"),
         GLMakie = (; px_per_unit=5)
 
     )
@@ -130,9 +131,7 @@ function get_tickstep(tickvalues)
     end
 
     dx = dx[1]
-    println(dx)
     place = floor(Int64, log10(abs(dx))) 
-    println(place)
 
     dx = dx * .10^(place)
 
@@ -148,62 +147,62 @@ end
 
 
 function get_subdivision(step)
-	if step ≈ 1
-		return 5
-	elseif step ≈ 2
-		return 4
-	elseif step ≈ 2.5
-		return 5
-	elseif step ≈ 3
-		return 3
-	elseif step ≈ 5
-		return 5
-	elseif step ≈ 10
-		return 5
-	else
-		@error "step $step not implemented"
-	end
+    if step ≈ 1
+        return 5
+    elseif step ≈ 2
+        return 4
+    elseif step ≈ 2.5
+        return 5
+    elseif step ≈ 3
+        return 3
+    elseif step ≈ 5
+        return 5
+    elseif step ≈ 10
+        return 5
+    else
+        @error "step $step not implemented"
+    end
 end
 
 
 function Makie.get_minor_tickvalues(i::SmartMinorTicks, scale::typeof(identity), tickvalues, vmin, vmax)
-	step, _ = get_tickstep(tickvalues)
-	subdivision = get_subdivision(step)
-	return Makie.get_minor_tickvalues(IntervalsBetween(subdivision), scale, tickvalues, vmin, vmax)
+    step, _ = get_tickstep(tickvalues)
+    subdivision = get_subdivision(step)
+    return Makie.get_minor_tickvalues(IntervalsBetween(subdivision), scale, tickvalues, vmin, vmax)
 end
 
 
 function Makie.get_minor_tickvalues(i::SmartMinorTicks, scale::typeof(log10), tickvalues, vmin, vmax)
-	step, place = get_tickstep(log10.(tickvalues))
-	step = 10. ^ (step * 10. ^ place)
-	subticks = []
-	
-	if step == 10
-		subticks = collect(2:9)
-	elseif step == 100
-		subticks = [10]
-	elseif step == 1000
-		subticks = [10, 100]
-	else
-		@error "difference $step not implemennted in log"
-	end
-	
-	expanded_tickvalues = [tickvalues[1]/step; tickvalues; tickvalues[end]*step]
+    step, place = get_tickstep(log10.(tickvalues))
+    step = 10. ^ (step * 10. ^ place)
+    subticks = []
+    
+    if step == 10
+            subticks = collect(2:9)
+    elseif step == 100
+            subticks = [10]
+    elseif step == 1000
+            subticks = [10, 100]
+    else
+            @error "difference $step not implemennted in log"
+    end
+    
+    expanded_tickvalues = [tickvalues[1]/step; tickvalues; tickvalues[end]*step]
 
-	minor_tick_values = Float64[]
-	for i in eachindex(expanded_tickvalues)
-		x0 = expanded_tickvalues[i]
+    minor_tick_values = Float64[]
+    for i in eachindex(expanded_tickvalues)
+        x0 = expanded_tickvalues[i]
 
-		for j in eachindex(subticks)
-			x = x0 * subticks[j]
-			if (x < vmin) | (x > vmax)
-				#
-			else
-				push!(minor_tick_values, x)
-			end
-		end
-	end
+        for j in eachindex(subticks)
+            x = x0 * subticks[j]
+            if (x < vmin) | (x > vmax)
+                    #
+            else
+                    push!(minor_tick_values, x)
+            end
+        end
+    end
 
-	@info minor_tick_values
-	return minor_tick_values
+    @debug minor_tick_values
+    return minor_tick_values
 end
